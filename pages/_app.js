5b0cdbf7878e20b88f3f34654736a3290e7ec080
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-08-20 20:12:54 -0400

    Wire Perfect Lawn into tool network

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
+import ToolNetworkLinks from "../components/ToolNetworkLinks";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToolNetworkLinks />
+    </>
+  );
 }
